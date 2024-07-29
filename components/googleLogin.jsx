3e1a17fc5e5d7ec623efa6dea3d8a1209010fd57
kamin72 <a1616001@gmail.com>
2024-07-29 23:03:27 +0800

    fixed

--- components/googleLogin.jsx
+++ components/googleLogin.jsx
@@ -4,7 +4,7 @@ export default function GoogleLogin() {
       <div
         id="g_id_onload"
         data-client_id="133688964144-uu4s377mpv9ctra1eo73v61th5336ng1.apps.googleusercontent.com"
-        data-login_uri="https://easy-eating-three.vercel.app/"
+        data-login_uri="https://easy-eating-three.vercel.app/mainPage"
         data-auto_prompt="false"></div>
       <div
         class="g_id_signin"
