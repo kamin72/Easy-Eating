7fd331c08609f4829e9daba743c3ebffd6a757ac
kamin72 <a1616001@gmail.com>
2024-07-29 22:36:21 +0800

    add

---
+++ pages/mainPage.jsx
@@ -0,0 +1,23 @@
+import Layout from "../components/layout";
+import Cards from "../components/card";
+import Head from "next/head";
+import Nav from "../components/nav";
+import Link from "next/link";
+
+export default function Main() {
+  return (
+    <>
+      <Head>
+        <title>Easy Eating</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <Nav />
+      <Layout>
+        <Cards />
+      </Layout>
+      {/* <Link href="/products">next page</Link> */}
+    </>
+  );
+}
