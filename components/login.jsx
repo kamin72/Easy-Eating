c7aed959a284192168a8dd967edb171fe71473ec
kamin72 <a1616001@gmail.com>
2024-07-29 22:18:52 +0800

    fixed

--- components/login.jsx
+++ components/login.jsx
@@ -1,25 +1,49 @@
+import Link from "next/link";
+import GoogleLogin from "./googleLogin";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 export default function LoginForm() {
+  const router = useRouter();
+  const [account, setAccount] = useState("aaaaaa");
+  const [psw, setPsw] = useState("111111");
+  // const [error, setError] = useState("");
+
+  function LoggedIn() {
+    if (account == "aaaaaa" && psw == "111111") {
+      localStorage.setItem("loggedInfo", JSON.stringify({ account, psw }));
+      router.push("/mainPage");
+    } else {
+      // setError("You have an error during logging in");
+      alert("Invalid account or password");
+    }
+  }
+
   return (
     <>
       <div className="border-2 flex flex-col w-[90%] m-auto py-[20px] px-[10px] sm:w-[50%]">
         <label className="font-bold">Account</label>
         <input
           type="text"
-          defaultValue="aaaaaa"
+          defaultValue={account}
           className="bg-slate-100  my-[10px] p-1"></input>
         <label className="mt-[10px] font-bold">Password</label>
         <input
           type="password"
-          defaultValue="111111"
+          defaultValue={psw}
           className="bg-slate-100 my-[10px] p-1"></input>
-        <div className="text-center pt-[20px]">
-          <button className="bg-sky-950 text-white w-[25%] mx-[20px] p-1 hover:bg-sky-500">
+        <div className="text-center py-[20px]">
+          <button
+            type="button"
+            className="bg-sky-950 text-white w-[25%] mx-[20px] p-1 hover:bg-sky-500"
+            onClick={LoggedIn}>
             Log in
           </button>
           <button className="bg-sky-950 text-white w-[25%] p-1 hover:bg-sky-500">
             Sign up
           </button>
         </div>
+        <GoogleLogin></GoogleLogin>
       </div>
     </>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,26 +4,52 @@ import Head from "next/head";
 import Nav from "../components/nav";
 import LoginForm from "../components/login";
 import Link from "next/link";
-import GoogleLogin from "../components/googleLogin";
 import Script from "next/script";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <Script src="https://accounts.google.com/gsi/client" async></Script>
-      <Nav />
-      <Layout>
-        <LoginForm />
+  const [memberData, setMemberData] = useState(null);
+
+  useEffect(() => {
+    const storage = localStorage.getItem("loggedInfo");
+    if (storage) {
+      setMemberData(true);
+    } else {
+      return setMemberData(false);
+    }
+  }, []);
+
+  if (!memberData) {
+    return (
+      <>
+        <Head>
+          <title>Log in</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"></meta>
+        </Head>
+        <Script src="https://accounts.google.com/gsi/client" async></Script>
+        <Nav />
+        <Layout>
+          <LoginForm />
+        </Layout>
+      </>
+    );
+  } else {
+    return (
+      <>
         <Head>
           <title>Easy Eating</title>
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"></meta>
         </Head>
-        <Cards />
-        <GoogleLogin />
-      </Layout>
-      {/* <Link href="/products">next page</Link> */}
-    </>
-  );
+        <Nav />
+        <Layout>
+          <Cards />
+        </Layout>
+        {/* <Link href="/products">next page</Link> */}
+      </>
+    );
+  }
 }
