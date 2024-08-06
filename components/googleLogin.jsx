6a9105b3077d6863b421bbb59dbd8efd903b294e
kamin72 <a1616001@gmail.com>
2024-08-06 11:17:21 +0800

    aa

--- components/googleLogin.jsx
+++ components/googleLogin.jsx
@@ -18,7 +18,7 @@ export default function GoogleLogin() {
         id="g_id_onload"
         data-client_id="133688964144-uu4s377mpv9ctra1eo73v61th5336ng1.apps.googleusercontent.com"
         data-context="signin"
-        data-login_uri="https://easy-eating-three.vercel.app"
+        data-login_uri="https://easy-eating-kamin72s-projects.vercel.app/"
         data-nonce=""
         data-itp_support="true"></div>
     </>
