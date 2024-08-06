50ec28d9c251ee4dd8a36a21f346e504edcab14f
kamin72 <a1616001@gmail.com>
2024-08-06 11:39:01 +0800

    ADD

--- components/googleLogin.jsx
+++ components/googleLogin.jsx
@@ -1,17 +1,6 @@
 import { useEffect } from "react";
 
 export default function GoogleLogin() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://accounts.google.com/gsi/client";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
   return (
     <>
       <div
