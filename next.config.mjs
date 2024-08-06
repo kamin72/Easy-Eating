ad95dfc0b496ecfe036187398c92256e1ae4e06d
kamin72 <a1616001@gmail.com>
2024-08-06 15:02:22 +0800

    add

--- next.config.mjs
+++ next.config.mjs
@@ -3,46 +3,6 @@ const nextConfig = {
   images: {
     domains: ["fakestoreapi.com"],
   },
-  headers: {
-    key: "Content-Security-Policy",
-    value:
-      "script-src 'self' https://accounts.google.com 'unsafe-inline' 'unsafe-eval';",
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' https://accounts.google.com https://vercel.live 'unsafe-inline' 'unsafe-eval';
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https:;
-              font-src 'self' data:;
-              connect-src 'self' https://vercel.live;
-              frame-src 'self' https://vercel.live;
-            `
-              .replace(/\s+/g, " ")
-              .trim(),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
