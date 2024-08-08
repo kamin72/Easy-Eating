801de4049f44607e6c863b806f1d6798bbef8be1
kamin72 <a1616001@gmail.com>
2024-08-08 14:39:18 +0800

    add

--- components/googleLogin.jsx
+++ components/googleLogin.jsx
@@ -7,7 +7,7 @@ export default function GoogleLogin() {
         id="g_id_onload"
         data-client_id="133688964144-uu4s377mpv9ctra1eo73v61th5336ng1.apps.googleusercontent.com"
         data-context="signin"
-        data-login_uri="http://localhost:3001/"
+        data-login_uri="https://easy-eating-three.vercel.app/"
         data-nonce=""
         data-itp_support="true"
         data-use_fedcm_for_prompt="true"></div>
